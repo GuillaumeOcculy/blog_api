e0f73220d6067789d20930a07640af91cd8c9777
Guillaume Occuly <occuly.guillaume@gmail.com>
2020-04-05 12:32:01 +0200

    Refactor Post serializer
    Remove list of users who liked posts

--- app/controllers/v1/posts_controller.rb
+++ app/controllers/v1/posts_controller.rb
@@ -2,19 +2,19 @@ class V1::PostsController < V1::BaseController
   before_action :authenticate_token!, only: [:create]
 
   def index
-    posts = Post.includes(:user, likes: :user).recent
-    render json: ::PostSerializer.new(posts, include: [:user, :'likes.user']).serialized_json
+    posts = Post.includes(:user, :likes).recent
+    render json: ::PostSerializer.new(posts, include: [:user]).serialized_json
   end
 
   def show
     post = Post.find(params[:id])
-    render json: ::PostSerializer.new(post, include: [:user, :'likes.user']).serialized_json
+    render json: ::PostSerializer.new(post, include: [:user]).serialized_json
   end
 
   def create
     post = current_user.posts.build(post_params)
     if post.save
-      render json: ::PostSerializer.new(post, include: [:user, :'likes.user']).serialized_json, status: :created
+      render json: ::PostSerializer.new(post, include: [:user]).serialized_json, status: :created
     else
       invalid_resource!(post.errors)
     end
--- app/serializers/post_serializer.rb
+++ app/serializers/post_serializer.rb
@@ -2,6 +2,6 @@ class PostSerializer
   include FastJsonapi::ObjectSerializer
   attributes :body, :created_at
 
-  belongs_to :user, record_type: :creator
+  belongs_to :user
   has_many :likes
 end
