8784be8fc06c313ae40bcb019ca465e92a31b877
Guillaume Occuly <occuly.guillaume@gmail.com>
2020-04-18 22:04:28 +0200

    Add Patch cors

--- config/application.rb
+++ config/application.rb
@@ -39,7 +39,7 @@ module App
     config.middleware.insert_before 0, Rack::Cors do
       allow do
         origins '*'
-        resource '*', headers: :any, methods: [:get, :post, :delete, :options]
+        resource '*', headers: :any, methods: [:get, :post, :patch, :delete, :options]
       end
     end
   end
